926376d611a7fdb4b1b0556d4cb8adfbfd5305e5
zsuper <piyushkumbhare10@gmail.com>
2025-09-02 19:17:33 -0700

    added meta.mainProgram attribute to avoid ambiguities

--- default.nix
+++ default.nix
@@ -10,6 +10,7 @@
       rev = "65c7e7101f8a2c83fae9181907e1b01094dd9be2";
       hash = "sha256-JO6b1Nbm82FLnTSK7sKdZVTNIUznwwFcFU/dxpHu5pM=";
     };
+    meta.mainProgram = "muxie";
     vendorHash = "sha256-CXd2j180T9ln21RTBCqCqdO32aeNIXHwiPRNcFPFt2I=";
   };
 }
