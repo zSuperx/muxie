14c4576740b6233c669f70771ffea4e4ad009a1f
zsuper <piyushkumbhare10@gmail.com>
2025-09-02 18:40:03 -0700

    Created flake.nix and default.nix to package for Nix(OS)

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Muxie";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forAllSystems ({ pkgs }: pkgs.callPackage ./. { });
+    };
+}
